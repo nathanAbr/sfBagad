431a61c21f316cdd810cf2b2fa756c94f481c557
nathanABR <nathan.abraham@outlook.fr>
2017-07-24 16:40:32 +0200

    add palmares tempalte

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -24,5 +24,13 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('public/evenements.html.twig');
     }
+    /**
+     * @Route("/palmares", name="palmares")
+     */
+    public function palmaresAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('public/palmares.html.twig');
+    }
 
 }
