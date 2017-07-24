ffad2de450e4fae9d67366f1a45d441467771e5f
nathanABR <nathan.abraham@outlook.fr>
2017-07-24 16:35:55 +0200

    add layout and event template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('public/layout.html.twig');
     }
+    /**
+     * @Route("/evenements", name="evenements")
+     */
+    public function evenementsAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('public/evenements.html.twig');
+    }
+
 }
