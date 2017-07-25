6b40c9ec49438c8243b84f07dde577c53e694727
nathanABR <nathan.abraham@outlook.fr>
2017-07-25 13:56:36 +0200

    add layout template espace membres

---
+++ src/AppBundle/Controller/EspaceMembresController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nathan
+ * Date: 25/07/2017
+ * Time: 13:36
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class EspaceMembresController extends Controller
+{
+    /**
+     * @Route("/test/", name="evenements")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('espaceMembres/layout.html.twig');
+    }
+
+}
