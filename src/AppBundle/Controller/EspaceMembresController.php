ed70f3ef418845bdb9a636a6183a7a6202503517
orlando1108 <erwan.raulo2015@campus-eni.fr>
2017-07-26 10:42:07 +0200

    layouts espaceMembres

--- src/AppBundle/Controller/EspaceMembresController.php
+++ src/AppBundle/Controller/EspaceMembresController.php
@@ -16,12 +16,53 @@ use Symfony\Component\HttpFoundation\Request;
 class EspaceMembresController extends Controller
 {
     /**
-     * @Route("/test/", name="evenements")
+     * @Route("/test/", name="acceuilTest")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('espaceMembres/layout.html.twig');
+    }
+
+
+    /**
+     * @Route("/sorties/", name="sorties")
+     */
+    public function sortiesAction(Request $request)
+    {
+        return $this->render('espaceMembres/sorties.html.twig');
+    }
+
+    /**
+     * @Route("/concours/", name="concours")
+     */
+    public function concoursAction(Request $request)
+    {
+        return $this->render('espaceMembres/concours.html.twig');
+    }
+
+    /**
+     * @Route("/cours/", name="cours")
+     */
+    public function coursAction(Request $request)
+    {
+        return $this->render('espaceMembres/cours.html.twig');
+    }
+
+    /**
+     * @Route("/reunions/", name="reunions")
+     */
+    public function reunionsAction(Request $request)
+    {
+        return $this->render('espaceMembres/reunions.html.twig');
+    }
+
+    /**
+     * @Route("/listeMembres/", name="listeMembres")
+     */
+    public function listeMembresAction(Request $request)
+    {
+        return $this->render('espaceMembres/listeMembres.html.twig');
     }
 
 }
