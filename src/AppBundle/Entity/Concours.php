632811a3f84aa95e2691f3b90135fc5f4a88ad08
Coline Martin <colinemartin95@gmail.com>
2017-07-27 16:51:45 +0200

    finalisation ajout d'un concours en base

--- src/AppBundle/Entity/Concours.php
+++ src/AppBundle/Entity/Concours.php
@@ -12,88 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Concours extends Evenement
 {
-
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="titre", type="string", length=255)
-     */
-    private $titre;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description", type="text")
-     */
-    private $description;
-
     /**
      * @ORM\OneToOne(targetEntity="Resultat", mappedBy="concours")
      */
     private $resultat;
-
-    /**
-     * Set titre
-     *
-     * @param string $titre
-     *
-     * @return Concours
-     */
-    public function setTitre($titre)
-    {
-        $this->titre = $titre;
-
-        return $this;
-    }
-
-    /**
-     * Get titre
-     *
-     * @return string
-     */
-    public function getTitre()
-    {
-        return $this->titre;
-    }
-
-    /**
-     * Set description
-     *
-     * @param string $description
-     *
-     * @return Concours
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * Get description
-     *
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * Set resultat
-     *
-     * @param \AppBundle\Entity\Resultat $resultat
-     *
-     * @return Concours
-     */
-    public function setResultat(\AppBundle\Entity\Resultat $resultat = null)
-    {
-        $this->resultat = $resultat;
-
-        return $this;
-    }
 
     /**
      * Get resultat
