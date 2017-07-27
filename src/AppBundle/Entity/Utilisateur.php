37a1f1775b85083094149939e0a3dd77d5172488
metasys16 <pereira.mickael1@gmail.com>
2017-07-27 09:35:58 +0200

    Surcharge template FOSUserBundle

--- src/AppBundle/Entity/Utilisateur.php
+++ src/AppBundle/Entity/Utilisateur.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Utilisateur
@@ -23,6 +24,55 @@ class Utilisateur extends BaseUser
     protected $id;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string")
+     */
+    protected $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string")
+     */
+    protected $prenom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_naissance", type="datetime")
+     */
+    protected $date_naissance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string")
+     */
+    protected $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cp", type="string")
+     */
+    protected $cp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string")
+     */
+    protected $ville;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="cotisation", type="boolean")
+     */
+    protected $cotisation;
+
+    /**
      * @ORM\OneToMany(targetEntity="Article", mappedBy="auteur")
      */
     private $articles;
@@ -30,6 +80,7 @@ class Utilisateur extends BaseUser
     /**
      * @ORM\ManyToOne(targetEntity="Instrument", inversedBy="utilisateurs")
      * @ORM\JoinColumn(name="instrument_id", referencedColumnName="id")
+     * @Assert\Type(type="AppBundle\Entity\Instrument")
      */
     private $instrument;
 
@@ -184,5 +235,173 @@ class Utilisateur extends BaseUser
     public function getUtilisateurEvenements()
     {
         return $this->utilisateurEvenements;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Utilisateur
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Utilisateur
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set dateNaissance
+     *
+     * @param \DateTime $dateNaissance
+     *
+     * @return Utilisateur
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->date_naissance = $dateNaissance;
+
+        return $this;
+    }
+
+    /**
+     * Get dateNaissance
+     *
+     * @return \DateTime
+     */
+    public function getDateNaissance()
+    {
+        return $this->date_naissance;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Utilisateur
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set cp
+     *
+     * @param string $cp
+     *
+     * @return Utilisateur
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get cp
+     *
+     * @return string
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Utilisateur
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set cotisation
+     *
+     * @param boolean $cotisation
+     *
+     * @return Utilisateur
+     */
+    public function setCotisation($cotisation)
+    {
+        $this->cotisation = $cotisation;
+
+        return $this;
+    }
+
+    /**
+     * Get cotisation
+     *
+     * @return boolean
+     */
+    public function getCotisation()
+    {
+        return $this->cotisation;
     }
 }
